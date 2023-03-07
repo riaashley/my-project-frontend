3f4ce8f10dded097f1cfa7e6cd7f1203d4a804c7
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 20:18:04 -0800

    home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return (
+        <h1 className="home_header">
+<center>Let's get Caffeinated!</center>
+        </h1>
+    )
+}
+
+export default Home;

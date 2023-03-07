3aafcc7c9473c2aaa3460b13c54557dabaec9e43
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 21:16:24 -0800

    updated changes

--- src/components/ShopContainer.js
+++ src/components/ShopContainer.js
@@ -1,12 +1,12 @@
 import React from "react";
+import ShopCard from "./ShopCard";
 
-
-function ShopContainer({ shops}){
+function ShopContainer({ shops, shopDelete}){
       
     return(
         <div className="shops-list">
             {shops.map((shop) => 
-                <ShopCard key={shop.id} shop={shop}  />
+                <ShopCard key={shop.id} shop={shop} shopDelete={shopDelete} />
             )}
 
         </div>

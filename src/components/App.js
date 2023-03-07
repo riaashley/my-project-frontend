b16ecac37613e94ef07cc03dbabb3d99160acbb4
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 20:53:43 -0800

    useeffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import Home from "../Home";
+import React, { useState, useEffect } from "react";
 import NavBar from "../NavBar";
 
 function App() {
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/shops")
+    .then((r) => r.json())
+    .then(data => setShops(data))
+  }, []);
+
   return (
-    <div className="App">
+    <div>
+       <NavBar />
      
     </div>
   );

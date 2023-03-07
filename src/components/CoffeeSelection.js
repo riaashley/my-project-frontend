be17c150c90d2048e903a5806d055c3b24c6cc59
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 21:04:31 -0800

    coffee category

--- src/components/CoffeeSelection.js
+++ src/components/CoffeeSelection.js
@@ -6,7 +6,7 @@ function CoffeeSelections({ drink }) {
         <div className="details">
             <img src={image_url} alt="coffee" width="80%" />
             <h2 className="drinks">{coffee_name}</h2>
-
+            <h3>{category} coffee</h3>
         </div>
     )
 }

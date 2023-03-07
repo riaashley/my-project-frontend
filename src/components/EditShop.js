72164a79d56865a7eef2062590cc222d0c968cf5
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 21:09:00 -0800

    edit form

--- src/components/EditShop.js
+++ src/components/EditShop.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 
-function AddShop({ shopAdded }){
-//name, image_url, location
+function EditShop({ shopEdit }){
+
     const [name, setName] = useState("");
     const [image, setImage] = useState("");
-    const [location, setLocation] = useState("");
+
 
     function handleNameChange(e){
         setName(e.target.value)
@@ -14,35 +14,23 @@ function AddShop({ shopAdded }){
         setImage(e.target.value)
     }
     
-    function handleLocationChange(e){
-        setLocation(e.target.value)
-    }
+
 
     function handleSubmit(e) {
         e.preventDefault()
-        const newShop = {
+        const update = {
             name: name,
             image: image,
-            location: location,
+
         }
         
-        fetch("http://localhost:9292/shops", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newShop)
-        })
-            .then((r) => r.json())
-            .then(data => shopAdded(data))
     }
 
     return(
         <div>
            <h1 className="newShop_header">
-        Add Your Shop For Some Caffeine Fun!
-               </h1><form className="add-activity" onSubmit={handleSubmit}>
-            <label>Business Name</label><br></br>
+                Edit Shop
+
                 <input type="text" onChange={handleNameChange} value={name} />
             <br></br>
             <label>Image Link</label><br></br>
@@ -51,10 +39,10 @@ function AddShop({ shopAdded }){
             <label>Location</label><br></br>
                 <input type="text" onChange={handleLocationChange} value={location} />
                 <br></br>
-                <button type="submit">Submit</button>
+
             </form>
         </div>
     )
 }
 
-export default AddShop;
+export default EditShop;

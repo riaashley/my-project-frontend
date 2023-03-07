de4a101fdb49fb2b3cf33b8e3821984ef8ae749a
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 20:49:33 -0800

    navbar edit

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function NavBar(){
     return (
-        
+        <div>
+             <ul className="navBar">
+                <li className="home">
+                    <Link to="/">
+                         Home
+                     </Link>
+                 </li>
+                <li className="shops">
+                    <Link to="/shops">
+                     Shops
+                    </Link>
+                 </li>  
+                 <li className="new_shop">
+                     <Link to="/shops/new">
+                         Add Shop
+                     </Link>
+                 </li>     
+            </ul>
+        </div>
     )
 }
+
+export default NavBar;

afdc6af23b10debbe49c0e34e476aebedba48e5d
Ria Legaspi <riaveluz@gmail.com>
2023-03-06 21:14:52 -0800

    app update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
-import Home from "../Home";
 import React, { useState, useEffect } from "react";
-
-import NavBar from "../NavBar";
+import Home from "./Home";
+import ShopContainer from "./ShopContainer";
+import AddShop from "./AddShop";
+import NavBar from "./NavBar";
+import ShopDetails from "./ShopDetails";
+import EditShop from "./EditShop";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   const [shops, setShops] = useState([]);
@@ -11,30 +15,29 @@ function App() {
     .then((r) => r.json())
     .then(data => setShops(data))
   }, []);
+ 
+    function shopDelete(deleteShop){
+      setShops([...shops, deleteShop])
+    }
 
-  function shopDelete(deleteShop){
-    setShops([...shops, deleteShop])
-  }
-  function shopAdded(newShop) {
-    setShops([...shops, newShop])
-  }
-  function shopEdit(editShop){
-    setShops([...shops, editShop])
-  }
-
+    function shopAdded(newShop) {
+      setShops([...shops, newShop])
+    }
+    function shopEdit(editShop){
+      setShops([...shops, editShop])
+    }
 
   return (
-    <div>
-       <NavBar />
-       <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/shops" element={<ShopContainer shopDelete={shopDelete} shops={shops}/>} />
-          <Route exact path="/shops/new" element={<AddShop shopAdded={shopAdded} />} />
-          <Route exact path="/shops/:id/edit" element={<EditShop shopEdit={shopEdit}/>} />
-        </Routes>
-     
-    </div>
-  );
+  <div>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/shops" element={<ShopContainer shopDelete={shopDelete} shops={shops}/>} />
+        <Route exact path="/shops/new" element={<AddShop shopAdded={shopAdded} />} />
+        <Route exact path="/shops/:id"element={<ShopDetails/>} />
+        <Route exact path="/shops/:id/edit" element={<EditShop shopEdit={shopEdit}/>} />
+      </Routes>
+  </div>
+  )
 }
-
 export default App;
